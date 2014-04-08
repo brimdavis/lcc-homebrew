b3ed51becd2234fae39d0e158d27af88342944da
brimdavis@gmail.com <brimdavis@gmail.com@bb4a67ec-b1ba-3d9d-57f7-f52677945bba>
2014-04-08 03:03:10 +0000

    yard.md:  - switch to lbsr for function calls  - moved cstart code to "link" stage instead of writing asm prefix from lcc  - swapped div/mod call operands  - fixed off-by-one-element in generated ASGN+B copy loop trailers  - function() rework:    - fixed incoming arg offset bugs in stack frame offset calculation code    - added stay-put code for incoming args in leaf routines    - round offset to quad boundary    - enforce minimum arg area of 16 bytes so varadic arg spills (r0:r3) can work properly    - spill all arg registers for varadic routines    - fix caller/callee dyslexia in generated comments    - reg arg copy loop now skips copy if src=dest for stay-put arg
    - commented out generated externs ( not supported in assembler yet )
    - use local label prefix for generated labels
    - generate hex constants with leading "$" instead of "0x"

--- win_setup_visual_studio.bat
+++ win_setup_visual_studio.bat
@@ -1,13 +1,20 @@
 ::
 :: setup paths for Visual Studio
 ::
-call "C:\Program Files\Microsoft Visual Studio 8\vc\vcvarsall.bat"
+
+::call "C:\Program Files\Microsoft Visual Studio 8\vc\vcvarsall.bat"
 ::call "C:\Program Files\Microsoft Visual Studio 10.0\vc\vcvarsall.bat"
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat"
 
 ::
 :: setup LCC environment variables
 ::
-set LCCDIR=C:\lcc\lcc-homebrew_vs8\bin
+
+::set LCCDIR=C:\lcc\lcc-homebrew_vs8\bin
+::set LCCDIR=C:\lcc\lcc-homebrew_vs10\bin
+
+set LCCDIR=C:\lcc\lcc-homebrew_vs10\bin
 
 
 
